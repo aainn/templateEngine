39b2c420ccc8e3daef8086f6fcf40173b57ff0e0
inana <inana_@gmx.de>
2018-03-07 09:33:16 +0100

    passed first variables in index.pug

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,19 @@
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: './bundle.js'
-    }
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+
+var config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Pug Template',
+      desc: 'A very long descriptions with test and bla',
+      posts: [{title: "1 bla bla"}, {title: "2 bla bla"}, {title: "3 bla bla"}, {title: "4 bla bla"}],
+      template: '!!pug-loader!src/index.pug',
+    }),
+  ]
 }
+module.exports = config;
